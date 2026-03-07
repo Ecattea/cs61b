f6c0475de47ca98ebc5077138cdcb0fa670f47b7
Ecattea <147585044+Ecattea@users.noreply.github.com>
2026-03-07 18:05:11 +0800

    hw03 completed

--- hw03/src/ArrayExercises.java
+++ hw03/src/ArrayExercises.java
@@ -2,13 +2,21 @@ public class ArrayExercises {
     /** Returns the second to last item in the given array.
      *  Assumes the array has at least 2 elements. */
     public static String secondToLastItem(String[] items) {
-        // TODO: Implement this method
-        return null;
+        return items[items.length - 2];
     }    
 
     /** Returns the difference between the minimum and maximum item in the given array */
     public static int minMaxDifference(int[] items) {
-        // TODO: Implement this method
-        return 0;           
+        int max = items[0];
+        int min = items[0];
+        for (int k = 1; k <= items.length - 1; k ++){
+            if (max <= items[k]){
+                max = items[k];
+            }
+            if (min >= items[k]){
+                min = items[k];
+            }
+        }
+        return max - min;
     }
 }
--- hw03/src/MapExercises.java
+++ hw03/src/MapExercises.java
@@ -7,21 +7,32 @@ public class MapExercises {
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        // TODO: Implement this method
-        return null;
+        String word = "abcdefghijklmnopqrstuvwxyz";
+        Map<Character, Integer> result = new TreeMap<>();
+        for (int i = 0; i <= 25; i ++){
+            result.put(word.charAt(i), i + 1);
+        }
+        return result;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        // TODO: Implement this method
-        return null;
+        Map<Integer, Integer> result = new TreeMap<>();
+        for (Integer element: nums){
+            int square = element * element; 
+            result.put(element, square);
+        }
+        return result;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        // TODO: Implement this method
-        return null;
+        Map<String, Integer> result = new TreeMap<>();
+        for (String word : words) {
+            result.put(word, result.getOrDefault(word, 0) + 1);
+        }
+        return result;
     }
 }
